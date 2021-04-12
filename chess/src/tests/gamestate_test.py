c121a5bb00e152607d8d2523e55f9bb02cf61b53
ItsTuukka <tuukka.ylostalo@helsinki.fi>
2021-04-12 17:50:01 +0300

    tests

---
+++ chess/src/tests/gamestate_test.py
@@ -0,0 +1,21 @@
+import unittest
+from gamestate import GameState
+
+class TestGameState(unittest.TestCase):
+    def setUp(self):
+        self.startingBoard = [["bR", "bN", "bB","bQ","bK","bB","bN","bR"],
+        ["bP","bP","bP","bP","bP","bP","bP","bP"],
+        ["","","","","","","",""],
+        ["","","","","","","",""],
+        ["","","","","","","",""],
+        ["","","","","","","",""],
+        ["wP","wP","wP","wP","wP","wP","wP","wP"],
+        ["wR","wN","wB","wQ","wK","wB","wN","wR"]]
+        self.GS = GameState()
+
+    def test_changeturn(self):
+        self.GS.changeBoardState("wP", (6,6), (6,5))
+        self.assertEqual(self.GS.moveWhite, False)
+    
+    def test_startBoard(self):
+        self.assertEqual(self.GS.boardstate, self.startingBoard)
--- chess/tasks.py
+++ chess/tasks.py
@@ -3,3 +3,20 @@ from invoke import task
 @task
 def start(ctx):
 	ctx.run('python3 src/main.py')
+
+@task
+def test(ctx):
+	ctx.run('pytest')
+	
+@task
+def coverage(ctx):
+	ctx.run('coverage run --branch -m pytest')
+	
+@task(coverage)
+def coverage_report(ctx):
+	ctx.run('coverage report -m')
+	
+@task(coverage)
+def coverage_html(ctx):
+	ctx.run('coverage html')
+
