a7074d16f3afb041fb7be02e5c4a40f42b779402
ItsTuukka <tuukka.ylostalo@helsinki.fi>
2021-04-12 20:48:20 +0300

    update

--- chess/tasks.py
+++ chess/tasks.py
@@ -10,13 +10,10 @@ def test(ctx):
 	
 @task
 def coverage(ctx):
-	ctx.run('coverage run --branch -m pytest')
+	ctx.run('coverage run --branch -m pytest src')
 	
 @task(coverage)
 def coverage_report(ctx):
-	ctx.run('coverage report -m')
-	
-@task(coverage)
-def coverage_html(ctx):
 	ctx.run('coverage html')
+	
 
